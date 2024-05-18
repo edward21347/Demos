806c3e9e484949806175dd52efb40369b5825844
edward21347 <158653937@qq.com>
2024-05-18 18:51:15 +0800

    jedis连接池

---
+++ Jedis-demo/src/main/java/JedisConnectionPool.java
@@ -0,0 +1,24 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisConnectionPool {
+    private static final JedisPool jedisPool;
+
+    static {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        //最大连接
+        jedisPoolConfig.setMaxTotal(8);
+        //最大空闲连接
+        jedisPoolConfig.setMaxIdle(8);
+        //最小空闲连接
+        jedisPoolConfig.setMinIdle(0);
+        //最长等待时间
+        jedisPoolConfig.setMaxWaitMillis(200);
+        jedisPool = new JedisPool(jedisPoolConfig,"192.168.37.128",6379,1000,"123321");
+    }
+
+    public static Jedis getJedis(){
+        return jedisPool.getResource();
+    }
+}
