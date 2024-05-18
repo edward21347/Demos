e334e8a0b440dc12d080fded4fe5e582cf70330d
edward21347 <158653937@qq.com>
2024-05-18 21:20:14 +0800

    springDataRedis - StringRedisTemplate的使用

--- SpringDataRedis/src/test/java/com/example/springdataredis/SpringDataRedisApplicationTest.java
+++ SpringDataRedis/src/test/java/com/example/springdataredis/SpringDataRedisApplicationTest.java
@@ -1,5 +1,6 @@
 package com.example.springdataredis;
 
+import com.alibaba.fastjson2.JSON;
 import com.example.springdataredis.pojo.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,13 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 @SpringBootTest
 class SpringDataRedisApplicationTest {
 
     @Autowired
     private RedisTemplate redisTemplate;
-
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     @BeforeEach
     void setUp() {
     }
@@ -39,5 +42,19 @@ class SpringDataRedisApplicationTest {
         valueOps.set("user", user);
         User user1 = (User)valueOps.get("user");
         System.out.println(user1);
+    }
+
+    @Test
+    public void testStringTemplate(){
+        User user = User.builder()
+                .age("18")
+                .name("阿枣").build();
+        String userStr = JSON.toJSONString(user);
+
+        ValueOperations valueOps = stringRedisTemplate.opsForValue();
+        valueOps.set("user", userStr);
+        String user1 = valueOps.get("user").toString();
+        User parsedUser = JSON.parseObject(user1, User.class);
+        System.out.println(parsedUser);
     }
 }
